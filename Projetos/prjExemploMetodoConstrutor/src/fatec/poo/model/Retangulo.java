0f2b8806917aba282bdc5519fd1df1ba7f8d181d
Nataly Rocha <94317936+nataly-costa@users.noreply.github.com>
2023-03-15 17:56:27 -0300

    Add files via upload

---
+++ Projetos/prjExemploMetodoConstrutor/src/fatec/poo/model/Retangulo.java
@@ -0,0 +1,47 @@
+package fatec.poo.model;
+
+/**
+ *
+ * @author Dimas
+ */
+public class Retangulo {
+    private double altura;
+    private double base;
+        private String unidadedeMedida;
+    
+// Definição do método construtor
+        
+    
+    public Retangulo(String uniMed){
+        unidadedeMedida = uniMed;
+    }
+    
+    public void setAltura(double a){
+        altura = a;
+    }
+    public void setBase(double b){
+        base = b;
+    }    
+    public double getAltura(){
+        return(altura);
+    }    
+    public double getBase(){
+        return(base);        
+    }    
+
+    public String getUnidadedeMedida() {
+        return unidadedeMedida;
+    }
+    
+    
+    
+    public double calcArea(){
+        return(base * altura);
+    }
+    public double calcPerimetro(){
+        return( 2 * (base + altura));
+    }
+    public double calcDiagonal(){
+        return(Math.sqrt(Math.pow(altura,2)+ Math.pow(base,2)));
+    }
+}
